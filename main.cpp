ba1ed9cdff5c98eeb632939394d750b9a9096fb2
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-10-16 01:08:55 +0400

    2ex

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include "iostream"
 
-int main(){
+enum cardinal_dirctions {
+    north = 25,
+    south,
+    east,
+    west
+};
 
-    int x = 8;
-    int y = 6;
+int main() {
 
-    std::cout << x - y << ' ' << x * y << x + y << std::endl;
+    std::cout << "Displaying directions and their symbolic values" << std::endl;
+
+    std::cout << "North: " << north << std::endl;
+    std::cout << "South:  " << south << std::endl;
+    std::cout << "East:  " << east << std::endl;
+    std::cout << "West: " << west << std::endl;
+
+    cardinal_dirctions wind_directions = south;
+
+    std::cout << "Variable wind_direction = " << wind_directions << std::endl;
 
     return 0;
 }
