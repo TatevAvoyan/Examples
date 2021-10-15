e09aeea03861cf006fe5a80c9559b152b68f2e0e
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-10-16 01:01:15 +0400

    first Example

---
+++ cmake-build-debug/CMakeFiles/Exaples.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exaples.dir/main.cpp.o"
+  "Exaples"
+  "Exaples.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Exaples.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "iostream"
+
+int main(){
+
+    int x = 8;
+    int y = 6;
+
+    std::cout << x - y << ' ' << x * y << x + y << std::endl;
+
+    return 0;
+}
