48f235b0732fa7450cddd304e89aa5915923d93c
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-10-25 21:54:50 +0400

    Example 6.6

--- main.cpp
+++ main.cpp
@@ -4,47 +4,13 @@ using namespace std;
 
 int main() {
 
-    enum DaysOfWeek {
-        Sunday = 0,
-        Monday,
-        Tuesday,
-        Wednesday,
-        Thursday,
-        Friday,
-        Saturday
-    };
-
-    cout << "Find what days of the week are named after!" << endl;
-    cout << "Enter a number for a day (Sunday = 0): " << endl;
-
-    int Day = Sunday;
-    cin >> Day;
-
-    switch (Day) {
-        case Sunday:
-            cout << "Sunday was named after the Sun" << endl;
-            break;
-        case Monday:
-            cout << "Monday was named after the Moon" << endl;
-            break;
-        case Tuesday:
-            cout << "Tuesday was named after Mars" << endl;
-            break;
-        case Wednesday:
-            cout << "Wednesday was named after Mercury" << endl;
-            break;
-        case Thursday:
-            cout << "Thursday was named after Jupiter" << endl;
-            break;
-        case Friday:
-            cout << "Friday was named after Venus" << endl;
-            break;
-        case Saturday:
-            cout << "Saturday was named after Saturn" << endl;
-            break;
-        default:
-            cout << "Wrong input, execute again" << endl;
-    }
+    cout << "Enter two numbers" << endl;
+    int Num1 = 0, Num2 = 0;
+    cin >> Num1;
+    cin >> Num2;
+    int Max = (Num1 > Num2) ? Num1 : Num2;
+    cout << "The greater of " << Num1 << " and "
+         << Num2 << " is: " << Max << endl;
 
     return 0;
 }
