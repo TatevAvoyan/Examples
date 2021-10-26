0a695cc9d9134d403064ce26408161bc977e30cd
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-10-27 01:17:14 +0400

    Example 6.10

--- main.cpp
+++ main.cpp
@@ -3,21 +3,21 @@ using namespace std;
 
 int main() {
 
-    char UserSelection = 'x';
-    do {
-        cout << "Enter the two integers: " << endl;
-        int Num1 = 0, Num2 = 0;
-        cin >> Num1;
-        cin >> Num2;
+    const int ARRAY_LENGTH = 5;
+    int MyInts[ARRAY_LENGTH] = {0};
 
-        cout << Num1 << " x " << Num2 << " = " << Num1 * Num2 << endl;
-        cout << Num1 << " + " << Num2 << " = " << Num1 + Num2 << endl;
+    cout << "Populate array of " << ARRAY_LENGTH << " integers" << endl;
 
-        cout << "Press x to exit(x) or any other key to recalculate" << endl;
-        cin >> UserSelection;
+    for(int ArrayIndex = 0; ArrayIndex < ARRAY_LENGTH; ++ArrayIndex){
+        cout << "Enter an integer for element " << ArrayIndex << ": ";
+        cin >> MyInts[ArrayIndex];
+    }
 
-    } while (UserSelection != 'x');
-        cout << "Goodbye!" << endl;
+    cout << "Displaying contents of the array: " << endl;
+
+    for(int ArrayIndex = 0; ArrayIndex < ARRAY_LENGTH; ++ArrayIndex){
+        cout << "Element  " << ArrayIndex << " = " << MyInts[ArrayIndex] << endl;
+    }
 
     return 0;
 }
