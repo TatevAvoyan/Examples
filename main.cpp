65db29bfb6f21dfded8182628edeb9d553b319bc
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-10-26 17:04:43 +0400

    Example 6.7

--- main.cpp
+++ main.cpp
@@ -4,13 +4,24 @@ using namespace std;
 
 int main() {
 
-    cout << "Enter two numbers" << endl;
+    JumpToPoint:
     int Num1 = 0, Num2 = 0;
+
+    cout << "Enter two integers: " << endl;
     cin >> Num1;
     cin >> Num2;
-    int Max = (Num1 > Num2) ? Num1 : Num2;
-    cout << "The greater of " << Num1 << " and "
-         << Num2 << " is: " << Max << endl;
+
+    cout << Num1 << " x " << Num2 << " = " << Num1 * Num2 << endl;
+    cout << Num1 << " + " << Num2 << " = " << Num1 + Num2 << endl;
+
+    cout << "Do yoy wish to perform another operation (y/n)?" << endl;
+    char Repeat = 'y';
+    cin >> Repeat;
+
+    if(Repeat == 'y')
+        goto JumpToPoint;
+
+    cout << "Goodbye!" << endl;
 
     return 0;
 }
