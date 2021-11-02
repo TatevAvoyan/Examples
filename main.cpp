e51c4d6c3ae2a8e5250cd5432e9f19d059e922be
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-11-03 02:49:52 +0400

    Example 8.4

--- main.cpp
+++ main.cpp
@@ -1,29 +1,26 @@
 #include "iostream"
-#include "algorithm"
-#include "vector"
 using namespace std;
-
-void DisplayNums(vector<int>& DynArray){
-    for_each (DynArray.begin(), DynArray.end(), \
-            [](int Element) {cout << Element << " ";} );
-}
 
 int main(){
 
-    vector<int> MyNumbers;
-    MyNumbers.push_back(501);
-    MyNumbers.push_back(-1);
-    MyNumbers.push_back(25);
-    MyNumbers.push_back(-35);
+    int Age = 30;
+    int DogsAge = 9;
 
-    DisplayNums(MyNumbers);
+    cout << "Integer Age = " << Age << endl;
+    cout << "Integer DogAge = " << DogsAge << endl;
 
-    cout << "Sorting them in descending order" << endl;
+    int *pInteger = &Age;
+    cout << "pInteger points to Age" << endl;
 
-    sort (MyNumbers.begin(), MyNumbers.end(), \
-    [](int Num1, int Num2) {return (Num2 < Num1); } );
+    cout << "pInteger = " << hex << pInteger << endl;
 
-    DisplayNums(MyNumbers);
+    cout << "*pInteger = " << dec << *pInteger << endl;
+
+    pInteger = &DogsAge;
+    cout << "pInteger points to DogsAge now" << endl;
+
+    cout << "pInteger = " << hex << pInteger << endl;
+    cout << "*pInteger = " << dec << *pInteger << endl;
 
     return 0;
 }
