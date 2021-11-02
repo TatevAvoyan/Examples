f1533a716c29251882dd8e2f5652d2de760dc08e
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-11-03 01:29:34 +0400

    Example 7.7

--- main.cpp
+++ main.cpp
@@ -1,20 +1,37 @@
 #include "iostream"
 using namespace std;
 
-int GetFibNumber (int FibIndex) {
-    if(FibIndex < 2)
-        return FibIndex;
-    else
-        return GetFibNumber(FibIndex - 1) + GetFibNumber(FibIndex -2);
-}
+const double Pi = 3.14159;
+
+double Area(double Radius);
+double Area(double Radius, double Height);
 
 int main() {
 
-    cout << "Enter 0-based index of desired Fibonacci Number: ";
-    int Index = 0;
-    cin >> Index;
+    cout << "Enter z for Cylinder, c for Circle: ";
+    char Choice = 'z';
+    cin >> Choice;
 
-    cout << "Fibonacci number is: " <<  GetFibNumber(Index) << endl;
+    cout << "Enter Radius: ";
+    double Radius = 0;
+    cin >> Radius;
+
+    if (Choice == 'z') {
+        cout << "Enter height: ";
+        double Height = 0;
+        cin >> Height;
+
+        cout << "Area of cylinder is: " << Area(Radius, Height) << endl;
+    } else {
+        cout << "Area of cylinder is: " << Area(Radius) << endl;
+    }
 
     return 0;
+}
+
+double Area(double Radius) {
+    return Pi * Radius * Radius;
+}
+double Area(double Radius, double Height){
+    return 2 * Area(Radius) +2 * Pi * Radius * Height;
 }
