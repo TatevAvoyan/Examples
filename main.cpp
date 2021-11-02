b3d1c4abebef3bc1349632eb1cefbf3acc2edaa3
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-11-02 23:36:35 +0400

    Example 7.2

--- main.cpp
+++ main.cpp
@@ -1,28 +1,27 @@
 #include "iostream"
 using namespace std;
 
+const double Pi = 3.14159;
+
+double SurfaceArea(double Radius, double Height);
+
 int main() {
 
-    const int NumsToCal = 5;
-    cout << "This program wil calculate " << NumsToCal \
-         << " Fibonacci Numbers at a time" << endl;
+    cout << "Enter the radius of the cylinder: ";
+    double InRadius = 0;
+    cin >> InRadius;
 
-    int Num1 = 0, Num2 = 1;
-    char WantMore = '\0';
-    cout << Num1 << " " << Num2 << " ";
+    cout << "Enter the height of the cylinder: ";
+    double InHeight = 0;
+    cin >> InHeight;
 
-    do {
-        for (int Index = 0; Index < NumsToCal; ++Index) {
-            cout << Num1 + Num2 << " ";
-
-            int Num2Temp = Num2;
-            Num2 = Num1 + Num2;
-            Num1 = Num2Temp;
-        }
-        cout << endl << "Do you want more numbers (y/n)? ";
-        cin >> WantMore;
-    } while (WantMore == 'y');
-    cout << "Goodbye!" << endl;
+    cout << "Surface Area: " << SurfaceArea(InRadius, InHeight) << endl;
 
     return 0;
+}
+
+double SurfaceArea(double Radius, double Height) {
+
+    double Area = 2 * Pi * Radius * Radius + 2 * Pi * Radius * Height;
+    return Area;
 }
