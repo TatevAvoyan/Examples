c5012f0acebc8e5766eaa3519efb2b22c5918842
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-11-03 01:57:17 +0400

    Example 7.8

--- main.cpp
+++ main.cpp
@@ -1,37 +1,27 @@
 #include "iostream"
 using namespace std;
 
-const double Pi = 3.14159;
+void DisplayArray(int Numbers[], int Length){
+    for (int Index = 0; Index < Length; ++Index) {
+        cout << Numbers[Index] << " ";
+    }
+    cout << endl;
+}
 
-double Area(double Radius);
-double Area(double Radius, double Height);
+void DisplayArray(char Characters[], int Length){
+    for (int Index = 0; Index < Length; ++Index) {
+        cout << Characters[Index] << " ";
+    }
+    cout << endl;
+}
 
 int main() {
 
-    cout << "Enter z for Cylinder, c for Circle: ";
-    char Choice = 'z';
-    cin >> Choice;
+    int MyNumbers[4] = {24, 58, -1, 245};
+    DisplayArray(MyNumbers, 4);
 
-    cout << "Enter Radius: ";
-    double Radius = 0;
-    cin >> Radius;
-
-    if (Choice == 'z') {
-        cout << "Enter height: ";
-        double Height = 0;
-        cin >> Height;
-
-        cout << "Area of cylinder is: " << Area(Radius, Height) << endl;
-    } else {
-        cout << "Area of cylinder is: " << Area(Radius) << endl;
-    }
+    char MyStatement[7] = {'H', 'e', 'l', 'l', 'o', '!', '\0'};
+    DisplayArray(MyStatement, 7);
 
     return 0;
-}
-
-double Area(double Radius) {
-    return Pi * Radius * Radius;
-}
-double Area(double Radius, double Height){
-    return 2 * Area(Radius) +2 * Pi * Radius * Height;
 }
