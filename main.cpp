087ed2d72f9ce694b646d6fbb0f82479b8e2ff0f
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-11-03 15:44:10 +0400

    Example 8.13

--- main.cpp
+++ main.cpp
@@ -3,30 +3,18 @@ using namespace std;
 
 int main(){
 
-    cout << "How many integers you wish to enter?";
-    int InputNums = 0;
-    cin >> InputNums;
+  int* pTemperature;
 
-    int* pNumbers = new int [InputNums];
-    int* pCopy = pNumbers;
+  cout << "Is it sunny (y/n)?" << endl;
+  char UserInput = 'y';
+  cin >> UserInput;
 
-    cout << "Successfully allocated memory for " << InputNums << " integers" << endl;
-
-    for (int Index = 0; Index < InputNums; ++Index) {
-        cout << "Enter number " << Index << ": ";
-        cin >> *(pNumbers + Index);
-    }
-
-    cout << "Displaying all numbers input: " << endl;
-    for (int Index = 0, int* pCopy = pNumbers;
-             Index < InputNums; ++Index)
-        cout << *(pCopy++) << " ";
-
-    cout << endl;
-
-    delete[] pNumbers;
-
-
+  if (UserInput == 'y'){
+      pTemperature = new int;
+      *pTemperature = 30;
+  }
+  cout << "Temperature is: " << *pTemperature;
+  delete pTemperature;
 
     return 0;
 }
